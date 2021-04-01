1e4c01a8352775937354e6273f69d3cafc3e8ca5
Iago Zamorano <iagozamo@ucm.es>
2021-04-01 11:17:42 +0200

    cache changed

--- scripts/artwork_similarity.py
+++ scripts/artwork_similarity.py
@@ -8,20 +8,35 @@ class CachedSimilarity():
     def computeSimilarity(self, A, B):
         raise NotImplementedError("Each subclass must implement this method")
 
-    def __searchEntity(self, Entity):
+    def __findSimilarity(self, A, B):
+        if A in self._cacheFile_ and B in self._cacheFile_[A].keys():
+            return self._cacheFile_[A][B], (A, B)
+        if B in self._cacheFile_ and A in self._cacheFile_[B].keys():
+            return self._cacheFile_[B][A], (B, A)
+        return None, None    
+
+    def __lenDict(self, Entity):
         if Entity not in self._cacheFile_:
-            sims = dict()
-            for other in iter(self._cacheFile_):
-                sims |= { other : self.computeSimilarity(Entity, other) }
-            self._cacheFile_[Entity] = sims
-        return self._cacheFile_[Entity]
+            self._cacheFile_[Entity] = dict()
+        return len(self._cacheFile_[Entity])
     
-    def getSimilarity(self, A, B):
-        dict_a = self.__searchEntity(A)
-        dict_b = self.__searchEntity(B)
-        if B in dict_a.keys():
-            return dict_a[B]
-        return dict_b[A]
+    def __storeSim(self, A, B, sim):
+        len_a = self.__lenDict(A)
+        len_b = self.__lenDict(B)
+        if len_a < len_b:
+            self._cacheFile_[A] |= {B : sim}
+        else:
+            self._cacheFile_[B] |= {A : sim}
+
+    def getSimilarity(self, A, B, recompute=False):
+        sim, coord = self.__findSimilarity(A, B)
+        if sim is None or recompute:
+            sim = self.computeSimilarity(A, B)
+            if coord is None:
+                self.__storeSim(A, B, sim)
+            else:
+                self._cacheFile_[coord[0]] |= { coord[1] : sim }
+        return sim
 
     def close(self):
         self._cacheFile_.sync()
@@ -121,7 +136,7 @@ from my_sparql import PropertyRetreiver
 class SizeSimilarity(CachedSimilarity):
 
     def __init__(self, cache_dir='../data/cache'):
-        super().__init__('size', cache_dir)
+        super().__init__('picture.size', cache_dir)
         self.__heightRetriever__ = PropertyRetreiver(['P2048'])
         self.__widthRetriever__ = PropertyRetreiver(['P2049'])
 
