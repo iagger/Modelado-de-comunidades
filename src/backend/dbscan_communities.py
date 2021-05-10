e86437149157bdda52b7a542887e822812ebeeed
Iago Zamorano <iagozamo@ucm.es>
2021-05-10 14:09:25 +0200

    dbscan log

--- src/backend/dbscan_communities.py
+++ src/backend/dbscan_communities.py
@@ -3,26 +3,26 @@ import numpy as np
 import pandas as pd
 from setup import PATHS, PARAMS
 from sklearn.cluster import dbscan
-from sklearn.metrics import silhouette_score
 from users_similarity import JaccardUserSimilarity
-from genal import GeneticAlgorithm
-from artwork_similarity import Partial_Similarities
 
-
+logging.basicConfig(filename="data/log/dbscanResults.log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s -> %(message)s')
 
 users = np.array(pd.read_csv(PATHS['USERS_DATA'])['userId']).reshape(-1,1)
-eps = .2
+eps = .25
+minClusterSize = 2
 mode = 5
-weights = [.4,  # Depicts
-           .15, # Size
-           .15, # DominantColor
-           .15, # Artist
-           .15] # MSE
+weights = [60, # Depicts
+           10, # Size
+           10, # DominantColor
+           10, # Artist
+           10] # MSE
 
 sim = JaccardUserSimilarity(mode=mode, weights=weights)
 def usersDistance(u1, u2):
     return 1 - sim.getSimilarity(u1[0], u2[0])
 
-labels = dbscan(X=users, eps=eps, metric=usersDistance, n_jobs=-1)[1]
+logging.info("Run DBSCAN with PARAMS [eps = " + str(eps) + ", minClusterSize = " + str(minClusterSize) + ", mode = " + str(mode) + ", weights = " + str(weights) + "]")
 
-print(labels)
+labels = dbscan(X=users, eps=eps, min_samples=minClusterSize, metric=usersDistance, n_jobs=-1)[1]
+
+logging.debug("Labels [" + str(labels) + "]")
